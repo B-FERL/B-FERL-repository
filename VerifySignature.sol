bdab58e0f587f916389ec8c95141aacf1d152691
Amirtha <90586089+Ami-Varsh@users.noreply.github.com>
2021-12-21 07:18:42 +0530

    Verifying Digital signature
    Checks the authenticity of the message sent

---
+++ VerifySignature.sol
@@ -0,0 +1,57 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.6.0;
+
+contract VerifySignature {
+    
+    function getEthSignedMessageHash(bytes32 _messageHash)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
+            );
+    }
+
+    function verify(
+        bytes32 messageHash,
+        bytes memory signature,
+        address _signer
+    ) public pure returns (bool) {
+
+        return recoverSigner(messageHash, signature) == _signer;
+    }
+
+    function recoverSigner(bytes32 messageHash, bytes memory _signature)
+        internal
+        pure
+        returns (address)
+    {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+
+        return ecrecover(messageHash, v, r, s);
+    }
+
+    function splitSignature(bytes memory sig)
+        internal
+        pure
+        returns (
+            bytes32 r,
+            bytes32 s,
+            uint8 v
+        )
+    {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // implicitly return (r, s, v)
+    }
+}
